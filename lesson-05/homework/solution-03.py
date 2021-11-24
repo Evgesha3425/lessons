412df5b7511f359df987f1c44566d12974a2840f
Yauheni Eidelman <yauheni.skills@gmail.com>
2021-11-24 19:44:37 +0300

    Exercise solution-03.py - easy solution

--- lesson-05/homework/solution-03.py
+++ lesson-05/homework/solution-03.py
@@ -5,15 +5,12 @@
 из ГитХаба из вашего репозитория: https://raw.githubusercontent.com/Evgesha3425/lessons/master/Licence
 """
 
-# Копировал с чужого репозитория. Пока что не получилось разобраться
-
-from pathlib import Path
 import requests
 
 
-def load_file(link, name_in_system):
-    filename = Path(name_in_system)
-    response = requests.get(link)
-    filename.write_bytes(response.content)
+def download_file(link, name):
+    r = requests.get(link)
+    open(name, 'wb').write(r.content)
 
-load_file('https://raw.githubusercontent.com/Evgesha3425/lessons/master/Licence', 'load_license_file.txt')
+
+download_file('https://raw.githubusercontent.com/Evgesha3425/lessons/master/Licence', 'lICENCE')
